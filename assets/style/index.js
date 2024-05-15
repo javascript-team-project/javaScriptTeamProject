757189e27b31c0cc0c05f18f4ba8cf3c898e4c3c
GongHanna <han_nah2853@naver.com>
2024-05-15 21:23:31 +0900

    hanna

--- assets/style/index.js
+++ assets/style/index.js
@@ -1,6 +1,6 @@
 // 스크롤 시 header 변경
 $(window).on('scroll', () => {
-  // const mainHt = $('main').height() * 0.2;
+  // const mainHt = $('.main-page-wrapper').height();
   if (scrollY > 100) {
     $('header').addClass('active');
   } else {
@@ -16,3 +16,36 @@ $('header.active .logo a').on('click', (e) => {
     behavior: 'smooth'
   })
 });
+
+// 자동으로 브랜드 소개 넘겨지기
+
+const $slideBtn = $('.brand-intro-wrapper .slide-button li');
+let currentIdx = 0;
+
+// pagenation
+$slideBtn.on('click', (e) => {
+  const idx = $(e.target).index();
+
+  $('.slide-wrapper').stop().animate({
+    marginLeft : -100 * idx + '%'
+  }, 700);
+
+  $slideBtn.removeClass('active');
+  $(e.target).addClass('active');
+
+  currentIdx = idx;
+});
+
+const autoSlide = () => {
+  currentIdx = (currentIdx + 1) % $('.slide-wrapper .slide-item').length;
+
+  $('.slide-wrapper').stop().animate({
+    marginLeft : -100 * currentIdx + '%'
+  }, 700);
+
+  $('.brand-intro-wrapper .slide-button li').eq(currentIdx).addClass('active').siblings().removeClass('active');
+}
+
+let mainSlide = setInterval(autoSlide, 3000);
+
+
