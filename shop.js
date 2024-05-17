28ead6c4917d6e6bbe23dc6069ffcf40ad83494b
GongHanna <han_nah2853@naver.com>
2024-05-17 19:16:37 +0900

    hanna

--- shop.js
+++ shop.js
@@ -1,15 +1,17 @@
 
 
 // 상품 배치 드롭다운 메뉴 (작동안됨)
-
-document.addEventListener('DOMContentLoaded', function () {
-  const btnArrange = document.querySelector('.btn-arrange');
-  const categoryMenu = btnArrange.querySelector('.category');
-
-  btnArrange.addEventListener('click', function () {
-    categoryMenu.classList.toggle('show');
-  });
+$('.btn-arrange li').on('click', () => {
+  $('.btn-arrange .category').toggleClass('show');
 });
+// document.addEventListener('DOMContentLoaded', function () {
+//   const btnArrange = document.querySelector('.btn-arrange');
+//   const categoryMenu = btnArrange.querySelector('.category');
+
+//   btnArrange.addEventListener('click', function () {
+//     categoryMenu.classList.toggle('show');
+//   });
+// });
 
 // 더보기 버튼 클릭 시 상품 추가 나열
 
