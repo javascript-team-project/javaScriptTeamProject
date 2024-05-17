a05f7b208e8ee32e7bf251bd77f4199d7b13f7c6
GongHanna <han_nah2853@naver.com>
2024-05-17 19:21:31 +0900

    hanna

--- shop.js
+++ shop.js
@@ -1,6 +1,6 @@
 
 
-// 상품 배치 드롭다운 메뉴 (작동안됨)
+// 상품 배치 드롭다운 메뉴 (작동됩니당 ♥)
 $('.btn-arrange li').on('click', () => {
   $('.btn-arrange .category').toggleClass('show');
 });
@@ -14,17 +14,23 @@ $('.btn-arrange li').on('click', () => {
 // });
 
 // 더보기 버튼 클릭 시 상품 추가 나열
-
-document.addEventListener('DOMContentLoaded', function() {
-  const moreBtn = document.querySelector('.more-btn');
-  const hiddenItems = document.querySelectorAll('.product-box.hidden');
-
-  moreBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    hiddenItems.forEach(item => {
-      item.classList.remove('hidden');
-    });
-    moreBtn.style.display = 'none'; // 더보기 버튼을 숨기기
-  });
+$('.sub-shop-wrapper .more-btn').on('click', (e) => {
+  e.preventDefault();
+  $('.sub-shop-wrapper .hidden').addClass('show-list');
+  $(e.target).hide();
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const moreBtn = document.querySelector('.more-btn');
+//   const hiddenItems = document.querySelectorAll('.product-box.hidden');
+
+//   moreBtn.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     hiddenItems.forEach(item => {
+//       item.classList.remove('hidden');
+//     });
+//     moreBtn.style.display = 'none'; // 더보기 버튼을 숨기기
+//   });
+// });
+
 
